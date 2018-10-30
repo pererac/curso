78efc8377d0a0ea60f972be16149e5d13c03473a
pererac <pererac@gmail.com>
2018-10-30 12:51:17 +0100

    subida7

--- js/desarrollo.js
+++ js/desarrollo.js
@@ -1,2 +1,6 @@
 import {sakudar } from "./sakudar.js"
+import (main) from './main.js'
 console.log("Pagina cargada");
+sakudar("Pepe");
+
+document.addEventListener("DOMContentLoaded", main);
---
+++ js/main.js
@@ -0,0 +1,9 @@
+export function main(){
+    document.querySelector("form1")
+    .addEventListener('submit', function(evt){
+        evt.preventDefault;
+        console.log('Entra en main');
+
+    })
+
+}
